00d6a1097e35c4b3ecdf84f8926f939409161ea2
SamuelPazz <samuel.paz@sptech.school>
2024-10-05 18:15:13 -0300

    fix: correções de nomes de campos e adição do arquivo sessao

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,132 +2,87 @@ const userModel = require("../models/userModel");
 
 //CREATE
 function cadastrarUsuario(req, res) {
-    let nome = req.body.nomeServer;
-    let email = req.body.emailServer;
-    let senha = req.body.senhaServer;
+  let nome = req.body.nomeServer;
+  let email = req.body.emailServer;
+  let senha = req.body.senhaServer;
 
-
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else {
-
-        userModel.cadastrarUsuario(nome, email, senha)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+  userModel
+    .cadastrarUsuario(nome, email, senha)
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log("\nHouve um erro ao realizar o cadastro", erro.sqlMessage);
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 //GET / READ
 function autenticar(req, res) {
-    let email = req.body.emailServer;
-    let senha = req.body.senhaServer;
+  let email = req.body.emailServer;
+  let senha = req.body.senhaServer;
 
-    // validações
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
-        userModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
-                    res.json({
-                        idFuncionario: resultadoAutenticar[0].idFuncionario,
-                        nomeFuncionario: resultadoAutenticar[0].nomeFuncionario,
-                        emailFuncionario: resultadoAutenticar[0].emailFuncionario
-                    })
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                });
-    }
+  userModel
+    .autenticar(email, senha)
+    .then(function (resultadoAutenticar) {
+      console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+      console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+      res.json({
+        usuario_id: resultadoAutenticar[0].usuario_id,
+        nome_usuario: resultadoAutenticar[0].nome,
+        email_usuario: resultadoAutenticar[0].email,
+      });
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "\nHouve um erro ao realizar o login! Erro: ",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 //UPDATE
 function atualizarSenha(req, res) {
-    let novaSenha = req.body.novaSenhaServer;
-    let idUsuario = req.body.idUsuarioServer;
+  let novaSenha = req.body.novaSenhaServer;
+  let usuario_id = req.body.usuario_idServer;
 
-    // validações
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else {
-
-        userModel.atualizarSenha(novaSenha, idUsuario)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+  userModel
+    .atualizarSenha(novaSenha, usuario_id)
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "\nHouve um erro ao atualizar a senha do usuário ! Erro: ",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 // DELETE
 function deletarUsuario(req, res) {
+  let usuario_deletar = req.body.usuario_idServer;
 
-    // validações
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else {
-        userModel.atualizarSenha(novaSenha, idUsuario)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+  userModel
+    .deletarUsuario(usuario_deletar)
+    .then(function (resultado) {
+      res.json(resultado);
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log("\nHouve um erro ao deletar o usuário", erro.sqlMessage);
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 module.exports = {
-    autenticar,
-    cadastrarUsuario,
-    atualizarSenha,
-    deletarUsuario
-}
+  autenticar,
+  cadastrarUsuario,
+  atualizarSenha,
+  deletarUsuario,
+};
