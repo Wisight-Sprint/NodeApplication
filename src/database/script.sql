0e7d9f93a8d5989ce5a615647411d214f332fbb3
SamuelPazz <samuel.paz@sptech.school>
2024-10-07 20:39:08 -0300

    feat: get id departamento por nome + cadastro

--- src/database/script.sql
+++ src/database/script.sql
@@ -2,13 +2,13 @@ create database if not exists wisight;
 use wisight;
 
 create table if not exists wisight.cidade_estado (
-  cidade_estado_id int primary key,
+  cidade_estado_id int primary key auto_increment,
   cidade varchar(45),
   estado varchar(45)
 );
 
 create table if not exists wisight.departamento (
-  departamento_id int primary key,
+  departamento_id int primary key auto_increment,
   nome varchar(45),
   fk_cidade_estado int,
     foreign key (fk_cidade_estado)
@@ -19,7 +19,7 @@ create table if not exists wisight.usuario (
   usuario_id int primary key auto_increment,
   nome varchar(45),
   email varchar(45),
-  numero varchar(45),
+  distintivo varchar(45),
   cargo varchar(45),
   senha varchar(45),
   fk_departamento int,
@@ -29,10 +29,10 @@ create table if not exists wisight.usuario (
 
 create table if not exists wisight.relatorio (
   relatorio_id int,
-  dt_dep date,
+  dt_ocorrencia date,
   fuga varchar(45),
-  camera_corporal tinyint,
-  problemas_mentais tinyint,
+  camera_corporal boolean,
+  problemas_mentais boolean,
   fk_departamento int,
   primary key (relatorio_id, fk_departamento),
     foreign key (fk_departamento)
@@ -42,7 +42,7 @@ create table if not exists wisight.relatorio (
 create table if not exists wisight.vitima (
   vitima_id int,
   idade int,
-  raca varchar(45),
+  etnia varchar(45),
   genero varchar(45),
   armamento varchar(45),
   fk_relatorio int,
