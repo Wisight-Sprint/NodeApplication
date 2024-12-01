2def8f43681adc6eecbe498f09cd6623edb7ef0b
Joao-Nocee <joao.noce@sptech.school>
2024-12-01 01:31:17 -0300

    Ao cadastrar um novo usuário em um departamento já existente ou novo, é enviado um email com informações para login. Quando um chamado é finalizado, um email é enviado com a resposta do chamado para a pessoa que abriu o chamado. No index, é possível enviar um email para os desenvolvedores com o nome, email, telefone e descrição do problema.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -167,6 +167,32 @@ function removerTutorialMapa(req, res) {
     });
 }
 
+function enviarEmail(req, res) {
+  
+  let de = req.body.deServer;
+  let para = req.body.paraServer;
+  let assunto = req.body.assuntoServer;
+  let mensagem = req.body.mensagemServer;
+
+  if (de == undefined) {
+    res.status(400).send("O destinatário está undefined!");
+  } else if (assunto == undefined) {
+    res.status(400).send("O assunto está undefined!");
+  } else if (mensagem == undefined) {
+    res.status(400).send("A mensagem está undefined!");
+  } else {
+    userModel
+      .enviarEmail(de, para, assunto, mensagem)
+      .then((resultado) => {
+        res.json({ mensagem: "E-mail enviado com sucesso!", resultado });
+      })
+      .catch((erro) => {
+        console.log("\nHouve um erro ao enviar o e-mail:", erro);
+        res.status(500).json({ erro: erro.message });
+      });
+  }
+}
+
 module.exports = {
   autenticar,
   cadastrarUsuario,
@@ -174,5 +200,6 @@ module.exports = {
   deletarUsuario,
   buscarUsuarioPorDepartamento,
   buscarExterno,
-  removerTutorialMapa
+  removerTutorialMapa,
+  enviarEmail
 };
