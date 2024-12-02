66ba891d8097f7c0bbfe8bb86b43a30b7ebea553
Joao-Nocee <joao.noce@sptech.school>
2024-12-02 17:21:48 -0300

    Ajuste no nome da tabela de insights

--- src/models/insightModel.js
+++ src/models/insightModel.js
@@ -5,7 +5,7 @@ function getInsight(userInsightKey, whereParam, joinParam) {
         "ACESSEI O INSIGHTS MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente."
     );
     let instrucaoSql = `
-    SELECT i.texto_insight FROM wisight.insights i 
+    SELECT i.texto_insight FROM wisight.insight i 
     ${joinParam}
     WHERE ${whereParam} = '${userInsightKey}' ORDER BY i.insight_id DESC LIMIT 1;`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
